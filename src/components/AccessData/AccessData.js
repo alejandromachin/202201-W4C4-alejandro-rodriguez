c47090b569e0f6927cc06f0bac07d889684c6209
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-03 19:22:31 +0100

    added accessdata form html

--- src/components/AccessData/AccessData.js
+++ src/components/AccessData/AccessData.js
@@ -4,41 +4,47 @@ const AccessData = () => {
       {" "}
       <div className="access-data">
         <form>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-            <small id="emailHelp" className="form-text text-muted">
-              We'll never share your email with anyone else.
-            </small>
+          <div className="form-row">
+            <div className="col">
+              <label htmlFor="name">First name</label>
+              <input
+                id="name"
+                type="text"
+                className="form-control"
+                placeholder="Enter name"
+              />
+            </div>
+            <div className="col">
+              <label htmlFor="lastname">Last name</label>
+              <input
+                id="lastname"
+                type="text"
+                className="form-control"
+                placeholder="Enter last name"
+              />
+            </div>
+            <div className="col">
+              <label htmlFor="email">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="col">
+              <label htmlFor="date">Date</label>
+              <input
+                type="date"
+                className="form-control"
+                id="date"
+                placeholder="Email"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
           </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-            />
-          </div>
-          <div className="form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-            />
-            <label className="form-check-label" htmlFor="exampleCheck1">
-              Check me out
-            </label>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
         </form>
       </div>
     </>
